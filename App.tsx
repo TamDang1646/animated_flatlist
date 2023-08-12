a98c1d0f6f733e0dcf7a6c600056bd50161021af
tamdev <dangtam1646@gmail.com>
2023-08-13 00:57:08 +0700

    animated_flatlist_with_RNAnimated

--- App.tsx
+++ App.tsx
@@ -7,41 +7,148 @@
 import React from "react";
 
 import {
-  FlatList,
+  Animated,
+  Image,
   SafeAreaView,
+  StatusBar,
   StyleSheet,
-  ViewToken,
+  Text,
+  View,
 } from "react-native";
-import {useSharedValue} from "react-native-reanimated";
 
-import {ListItem} from "./src/components/ListIItem";
+import {faker} from "@faker-js/faker";
 
-const data = new Array(50).fill(0).map((_, index)=> ({ id: index }));
-
+interface People {
+  id: number,
+  image: string,
+  name: string,
+  jobTitle: string,
+  email: string,
+}
+const DATA: People[] = [...Array(30).keys()].map((_, i) => {
+  return {
+    id: i,
+    image: faker.image.avatar(),
+    name: faker.person.fullName(),
+    jobTitle: faker.person.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
 const App = () => {
-  const viewableItems = useSharedValue<ViewToken[]>([]);
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+  // const viewableItems = useSharedValue<ViewToken[]>([]);
+  //! First Animated FlatList
+  // return (
+  //   <SafeAreaView style={styles.container}>
+  //     <Image source={require("./assets/images/bg.jpg")}
+  //       style={StyleSheet.absoluteFillObject} />
+  //     <FlatList
+  //       data={DATA}
+  //       onViewableItemsChanged={({viewableItems: vItems}) => {
+  //         // console.log(viewableItems);
+  //         viewableItems.value = vItems;
+  //       }}
+  //       keyExtractor={item => `index-${item.id}`}
+  //       contentContainerStyle={{
+  //         padding: SPACING,
+  //         paddingTop: StatusBar.currentHeight || 42,
+  //       }}
+  //       renderItem={({item}) => {
+  //         return <ListItem item={item} viewableItems={viewableItems} />;
+  //       }}
+  //     />
+  //   </SafeAreaView>
+  // );
 
+  // //! Second Animated FlatList
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={data}
-        contentContainerStyle={{ paddingTop: 40 }}
-        onViewableItemsChanged={({viewableItems: vItems}) => {
-          // console.log(viewableItems);
-          viewableItems.value = vItems;
-        }}
-        renderItem={({item}) => {
-          return <ListItem item= {item} viewableItems={viewableItems} />;
-        }}
-      />
+      <Image source={require("./assets/images/bg.jpg")}
+        style={StyleSheet.absoluteFillObject} />
+        <Animated.FlatList
+          data={DATA}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+            { useNativeDriver: true }
+          )}
+          keyExtractor={item => `index-${item.id}`}
+          contentContainerStyle={{
+            padding: SPACING,
+            paddingTop: StatusBar.currentHeight || 42,
+          }}
+          renderItem={({ item, index }) => {
+
+            const inputRange = [
+              -1,
+              0,
+              ITEM_SIZE * index,
+              ITEM_SIZE * (index + 10)
+            ]
+
+            const opacityInputRange = [
+              -1,
+              0,
+              ITEM_SIZE * index,
+              ITEM_SIZE * (index + 2)
+            ]
+
+            const scale = scrollY.interpolate({
+              inputRange,
+              outputRange: [1, 1, 1, 0]
+            })
+
+            const opacity = scrollY.interpolate({
+              inputRange: opacityInputRange,
+              outputRange: [1, 1, 1, 0]
+            })
+
+            return <Animated.View style={[{
+              flex: 1,
+              flexDirection: "row",
+              padding: SPACING,
+              backgroundColor: "rgba(255,255,255,1)",
+              marginBottom: SPACING,
+              borderRadius: 12,
+              shadowColor: "#000",
+              shadowOffset: {
+                width: 0,
+                height: 10,
+              },
+              shadowOpacity: 0.8,
+              shadowRadius: 10,
+              elevation: 9,
+              transform: [{ scale }],
+              opacity
+            }]}>
+              <Image source={{ uri: item.image }}
+                style={{
+                  width: AVATAR_SIZE,
+                  height: AVATAR_SIZE,
+                  marginRight: SPACING / 2,
+                  borderRadius: AVATAR_SIZE / 2,
+                  overflow: "hidden"
+                }}
+              />
+              <View style={{ flex: 1, backgroundColor: "tranperent" }}>
+                <Text style={{ fontSize: 22, fontWeight: "700" }}>{item.name}</Text>
+                <Text style={{ fontSize: 18, opacity: .7, flex: 1 }} numberOfLines={1} ellipsizeMode="tail">{item.jobTitle}</Text>
+                <Text style={{ fontSize: 14, opacity: .8, color: "#0099cc" }}>{item.email}</Text>
+              </View>
+            </Animated.View>;
+          }}
+        />
     </SafeAreaView>
-  );
+  )
 };
 export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "rgba(255,255,255,0.7)",
   },
   viewItem: {
     height: 80,
--- src/components/ListIItem.tsx
+++ src/components/ListIItem.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import {
-  StyleSheet,
+  Image,
+  Text,
+  View,
   ViewToken,
 } from "react-native";
 import Animated, {
@@ -10,11 +12,23 @@ import Animated, {
   withTiming,
 } from "react-native-reanimated";
 
-type ListItemProps = {
-    viewableItems?: SharedValue<ViewToken[]>;
-    item?: { id: number }
+interface People {
+    id: number,
+    image: string,
+    name: string,
+    jobTitle: string,
+    email: string,
 }
+type ListItemProps = {
+    viewableItems: SharedValue<ViewToken[]>;
+    item: People
+}
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+
+
 export const ListItem: React.FC<ListItemProps> = React.memo(({ item, viewableItems }) => {
+    
     const rStyle = useAnimatedStyle(() => {
         const isVisible = viewableItems?.value
             .filter(item => item.isViewable)
@@ -23,24 +37,42 @@ export const ListItem: React.FC<ListItemProps> = React.memo(({ item, viewableIte
             opacity: withTiming(isVisible ? 1 : 0),
             transform: [
                 {
-                    scale: withTiming(isVisible ? 1 : 0)
+                    scale: withTiming(isVisible ? 1 : 0.5)
                 }
             ]
         }
     })
-    return <Animated.View style={[styles.viewItem, rStyle]} />;
 
-});
-const styles = StyleSheet.create({
-    container: {
+    return <Animated.View style={[{
         flex: 1,
-    },
-    viewItem: {
-        height: 80,
-        width: "90%",
-        backgroundColor: "#78CAD2",
-        alignSelf: "center",
-        borderRadius: 16,
-        marginTop: 20,
-    },
+        flexDirection: "row",
+        padding: SPACING,
+        backgroundColor: "rgba(255,255,255,1)",
+        marginBottom: SPACING,
+        borderRadius: 12,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.8,
+        shadowRadius: 10,
+        elevation: 9,
+    },rStyle]}>
+        <Image source={{ uri: item.image }}
+            style={{
+                width: AVATAR_SIZE,
+                height: AVATAR_SIZE,
+                marginRight: SPACING / 2,
+                borderRadius: AVATAR_SIZE / 2,
+                overflow: "hidden"
+            }}
+        />
+        <View style={{ flex: 1, backgroundColor: "tranperent" }}>
+            <Text style={{ fontSize: 22, fontWeight: "700" }}>{item.name}</Text>
+            <Text style={{ fontSize: 18, opacity: .7, flex: 1 }} numberOfLines={1} ellipsizeMode="tail">{item.jobTitle}</Text>
+            <Text style={{ fontSize: 14, opacity: .8, color: "#0099cc" }}>{item.email}</Text>
+        </View>
+    </Animated.View>;
+
 });
